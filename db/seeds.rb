8039182bbce8c5cfb91c2d2ccf4c2e6688649af6
John-Plough <jplough@gmail.com>
2025-02-27 11:24:48 -0500

    connected product and supplier

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+supplier = Supplier.create(name: "JumboCorp", email: "jc@jc.com", phone_number: "(231) 555-4040")
 
-product = Product.new(name: "circus peanuts", price: 5, image_url: "https://crazyoutletcandy.com/cdn/shop/files/61YQ2ZvZ8eL._SL1126_1200x1200.jpg?v=1725981736", description: "one pound of circus peanuts")
+supplier = Supplier.create(name: "Yarmouth Industries", email: "yar@gmail.com", phone_number: "(231) 555-5050")
 
-product = Product.new(name: "garden gnome", price: 33, image_url: "https://www.bitsandpieces.com/cdn/shop/files/41351_1220x1220_crop_center.webp?v=1712756640", description: "gnome lounging in a bath")
+# product = Product.create(name: "circus peanuts", price: 5, image_url: "https://crazyoutletcandy.com/cdn/shop/files/61YQ2ZvZ8eL._SL1126_1200x1200.jpg?v=1725981736", description: "one pound of circus peanuts")
 
-product = Product.new(name: "umbrella", price: 12, image_url: "https://republiclifestyle.co.za/wp-content/uploads/2022/06/rain-umbrellas-all-colours-and-styles3.jpg", description: "red umbrella")
+# product = Product.create(name: "garden gnome", price: 33, image_url: "https://www.bitsandpieces.com/cdn/shop/files/41351_1220x1220_crop_center.webp?v=1712756640", description: "gnome lounging in a bath")
+
+# product = Product.create(name: "umbrella", price: 12, image_url: "https://republiclifestyle.co.za/wp-content/uploads/2022/06/rain-umbrellas-all-colours-and-styles3.jpg", description: "red umbrella")
