37059c616832035b57021ad465ca1429e2b40aad
John-Plough <jplough@gmail.com>
2025-03-18 16:41:18 -0400

    fixed orders jbuilder file

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,12 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = current_user.orders
+    render :index
+  end
+
   def create
     product = Product.find_by(id: params[:product_id])
+
     subtotal = product.price * params[:quantity].to_i
     tax = product.tax * params[:quantity].to_i
     total = subtotal + tax
@@ -8,21 +14,20 @@ class OrdersController < ApplicationController
     @order = Order.create(
       user_id: current_user.id,
       product_id: params[:product_id],
-      quantity: quantity,
+      quantity: params[:quantity],
       subtotal: subtotal,
       tax: tax,
       total: total
     )
-    render :show
+    if @order.valid?
+      render :show
+    else
+      render json: { errors: @order.errors.full_messages }, status: 422
+    end
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
+    @order = current_user.orders.find_by(id: params[:id])
     render :show
-  end
-
-  def index
-    @orders = Order.all
-    render :index
   end
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,26 +1,30 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  test "create" do
-    assert_difference "Order.count", 1 do
-      post "/orders.json", params: { name: "lake", width: 800, height: 600 }
-      assert_response 200
-    end
-  end
-
-  test "show" do
-    get "/orders/#{Order.first.id}.json"
-    assert_response 200
-
-    data = JSON.parse(response.body)
-    assert_equal [ "user_id", "product_id", "quantity", "subtotal", "tax", "created_at", "updated_at" ], data.keys
+  setup do
+    @user = User.create(name: "Test", email: "test@test.com", password: "password")
+    @order = Order.create(user_id: @user.id, product_id: Product.first.id, quantity: 10)
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
   end
 
   test "index" do
     get "/orders.json"
     assert_response 200
+  end
+
+  test "create" do
+    assert_difference "Order.count", 1 do
+      post "/orders.json",
+        params: { product_id: Product.first.id, quantity: 10 }
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/orders/#{@order.id}.json"
+    assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal Order.count, data.length
+    assert_equal [ "id", "quantity", "subtotal", "tax", "total", "product", "created_at", "updated_at" ], data.keys
   end
 end
